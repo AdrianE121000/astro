e836dac1a12497f258d633b78fb4c81dfe23aaac
AdrianE121000 <paracosa3s@gmail.com>
2024-04-11 11:32:35 -0400

    update PlayButton

--- spotify-clone/src/components/CardPlayButton.jsx
+++ spotify-clone/src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from './Player';
 import { usePlayerStore } from '../store/playerStore';
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
@@ -10,11 +10,10 @@ export function CardPlayButton({ id }) {
   const handleClick = () => {
     if (isPlayingPlaylist) {
       setIsPlaying(false);
-
       return;
     }
 
-    fetch(`api/get-info-playlist.json?id=${id}`)
+    fetch(`/api/get-info-playlist.json?id=${id}`)
       .then((res) => res.json())
       .then((data) => {
         const { songs, playlist } = data;
@@ -24,11 +23,17 @@ export function CardPlayButton({ id }) {
       });
   };
 
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5';
+
   return (
     <button
       onClick={handleClick}
-      className='card-play-button rounded-full bg-green-500 p-4'>
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+      className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400'>
+      {isPlayingPlaylist ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   );
 }
